8f1f3aae360c382d7873e34d74ce01b6ec309664
marimeireles <marian.meireles@gmail.com>
2018-05-20 15:53:14 -0300

    first commit

---
+++ requisidoresAPI/getDeputadoDespesas.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+deputadoID = json.load(open('deputadoID.json'))
+
+dados = []
+
+for ID in deputadoID:
+  r = requests.get('https://dadosabertos.camara.leg.br/api/v2/deputados/{}/despesas?ano=2017&ordem=ASC&ordenarPor=nomeFornecedor'.format(ID['id']))
+  dadosAbertos = r.json()
+  links = dadosAbertos['links']
+  links = {item['rel']: item['href'] for item in links}
+
+  dados.extend(dadosAbertos['dados'])
+
+  for dado in dados:
+    dado['idDeputado'] = ID['id']
+    print('criei um trem')
+
+  while True:
+    r = requests.get(links['next'])
+
+    dadosAbertos = r.json()
+    links = dadosAbertos['links']
+    links = {item['rel']: item['href'] for item in links}
+    dados.extend(dadosAbertos['dados'])
+
+    if 'next' not in links:
+      break
+
+with open('deputadoDespesas.json', 'a') as out:
+  out.write(json.dumps(dados))
---
+++ requisidoresAPI/getRequestsDeputadoID.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+r = requests.get('https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
+dadosAbertos = r.json()
+links = dadosAbertos['links']
+links = {item['rel']: item['href'] for item in links}
+
+dados = []
+dados.extend(dadosAbertos['dados'])
+
+
+while True:
+  r = requests.get(dadosAbertos['links'][1]['href'])
+  dadosAbertos = r.json()
+  links = dadosAbertos['links']
+  links = {item['rel']: item['href'] for item in links}
+  dados.extend(dadosAbertos['dados'])
+
+  if 'next' not in links:
+    break
+
+with open('deputadoID.json', 'w') as out:
+  out.write(json.dumps(dados))
